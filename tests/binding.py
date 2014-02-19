d410dafe318fdf8e954acb1ddb789939df8e8cbe
Alvaro J. Genial <genial@alva.ro>
2014-02-19 17:54:40 -0500

    Add python binding sample

--- tests/binding.py
+++ tests/binding.py
@@ -8,24 +8,44 @@ import synthesis
 print('Loaded synthesis; version: ' + synthesis.version())
 
 source = """
-foo: {{ foo }}; {% if foo %} yes {% else %} <p>no {% endif %}
-bar: {{ bar }}; {% if bar %} yes {% else %} <p>no {% endif %}
-qux: {{ qux }}; {% if qux %} yes {% else %} <p>no {% endif %}
-True: {{ True }}; {% if True %} yes {% else %} <p>no {% endif %}
-False: {{ False }}; {% if False %} yes {% else %} <p>no {% endif %}
-"": {{ "" }}; {% if "" %} yes {% else %} <p>no {% endif %}
-0: {{ 0 }}; {% if 0 %} yes {% else %} <p>no {% endif %}
 
-{% for k, v in "test" %} {{ k }} - {{ v }} {% endfor %}
+True:   {{ True }}; {% if True %} GOOD {% else %} BAD {% endif %}
+False:  {{ False }}; {% if False %} BAD {% else %} GOOD {% endif %}
 
-{% for k, v in qux %} {{ k }} - {{ v }} {% endfor %}
+0:      {{ 0 }}; {% if 0 %} BAD {% else %} GOOD {% endif %}
+42:     {{ 42 }}; {% if 42 %} GOOD {% else %} BAD {% endif %}
 
-{% for k, v in qux2 %} {{ k }} - {{ v }} {% endfor %}
+"":     {{ "" }}; {% if "" %} BAD {% else %} GOOD {% endif %}
+"test": {{ "test" }}; {% if "test" %} GOOD {% else %} BAD {% endif %}
 
-{% for k, v in empty1 %} {{ k }} - {{ v }} {% empty %} empty1 {% endfor %}
-{% for k, v in empty2 %} {{ k }} - {{ v }} {% empty %} empty2 {% endfor %}
+bool_true: {{ bool_true }}; {% if bool_true %} GOOD {% else %} BAD {% endif %}
+bool_false: {{ bool_false }}; {% if bool_false %} BAD {% else %} GOOD {% endif %}
+
+int_0: {{ int_0 }}; {% if int_0 %} BAD {% else %} GOOD {% endif %}
+int_42: {{ int_42 }}; {% if int_42 %} GOOD {% else %} BAD {% endif %}
+
+str_empty: {{ str_empty }}; {#% if str_empty %} BAD {% else %} GOOD {% endif %#}
+str_test: {{ str_test }}; {% if str_test %} GOOD {% else %} BAD {% endif %}
+
+list_empty: {{ list_empty }}; {% if list_empty %} BAD {% else %} GOOD {% endif %}
+list_12345: {{ list_12345 }}; {% if list_12345 %} GOOD {% else %} BAD {% endif %}
+
+dict_empty: {{ dict_empty }}; {% if dict_empty %} BAD {% else %} GOOD {% endif %}
+dict_abc: {{ dict_abc }}; {% if dict_abc %} GOOD {% else %} BAD {% endif %}
+
+{#% for k, v in str_empty %} BAD {% empty %} GOOD {% endfor %#}
+{% for k, v in list_empty %} BAD {% empty %} GOOD {% endfor %}
+{% for k, v in dict_empty %} BAD {% empty %} GOOD {% endfor %}
+
+"test": {% for v in "test" %}{{ v }}{% endfor %}
+str_test: {#% for v in str_test %}{{ v }}{% endfor %#}
+list_12345: {% for v in list_12345 %}{{ v }}{% endfor %}
+dict_abc: {% for v in dict_abc %}{{ v }}{% endfor %}
+
+dict_abc: {% for k, v in dict_abc.items %}{{ k }}:{{ v }}{% endfor %}
 
 {% comment %}
+
 {{ 0 or 0 }}
 {{ 0 and 0 }}
 {{ 0 or "foo" }}
@@ -37,7 +57,18 @@ False: {{ False }}; {% if False %} yes {% else %} <p>no {% endif %}
 {% endcomment %}
 """.encode('utf-8')
 
-context = {'foo': True, 'bar': 42, 'qux': [1,2,3,4,5], 'qux2': {'a': 'A', 'b': 'B', 'c': 'C'}, 'empty1': [], 'empty2': {}}
+context = {
+    'bool_true':  True,
+    'bool_false': False,
+    'int_0':      0,
+    'int_42':     42,
+    'str_empty':  "",
+    'str_test':   "test",
+    'list_empty': [],
+    'list_12345': [1, 2, 3, 4, 5],
+    'dict_empty': {},
+    'dict_abc':   {'a': 'A', 'b': 'B', 'c': 'C'},
+}
 template = synthesis.Template(source, 'django')
 
 print(template.render_to_string(context))

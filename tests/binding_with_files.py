2a91022562287a184e8d571f010e7aec1ca5fa78
Alvaro J. Genial <genial@alva.ro>
2014-02-26 23:03:09 -0500

    Add python binding test with files

---
+++ tests/binding_with_files.py
@@ -0,0 +1,31 @@
+##  (C) Copyright 2014 Alvaro J. Genial (http://alva.ro)
+##  Use, modification and distribution are subject to the Boost Software
+##  License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+##  http://www.boost.org/LICENSE_1_0.txt).
+
+import synthesis
+
+print('Loaded synthesis; version: ' + synthesis.version())
+
+directory = 'samples/django/templates/'
+source = open(directory + 'layout.html').read().encode('utf-8')
+
+
+class User:
+    def __init__(self):
+        self.is_staff = False
+        self.is_authenticated = True
+
+context = {
+    'request': {
+        'user': User(),
+    },
+    'STATIC_URL': '/static/',
+    'title': 'Default Title',
+    'messages': ["Foo", "Bar", "Qux"],
+}
+
+template = synthesis.Template(source, 'django', True, 'INVALID_VALUE', [directory])
+
+print(template.render_to_string(context))
+

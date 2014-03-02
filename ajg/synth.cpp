c3e43dd6b2921710c9a16b6e8620084305f5fff7
Alvaro J. Genial <genial@alva.ro>
2014-03-02 14:46:58 -0500

    Implement command-line program

--- ajg/synth.cpp
+++ ajg/synth.cpp
@@ -3,47 +3,43 @@
 //  License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt).
 
-#include <string>
-#include <vector>
 #include <cstdlib>
-#include <iostream>
-
-#include <boost/foreach.hpp>
-#include <boost/program_options.hpp>
-#include <boost/property_tree/ptree.hpp>
 
 #include <ajg/synth.hpp>
+#include <ajg/synth/command.hpp>
+#include <ajg/synth/processor.hpp>
 
+namespace {
 
-int main(int const argc, char const *const argv[]) {
-    // TODO: Parameterize on char type.
-    namespace po = boost::program_options;
-    po::options_description description("Usage");
-    typedef std::vector<std::string> paths;
+namespace synth = ajg::synth;
 
-    description.add_options()
-        ("help,h",                                                       "print help message")
-        ("version,v",                                                    "print library version")
-        ("input,i",       po::value<std::string>()->value_name("file"),  "template input (default: '-')")
-        ("output,o",      po::value<std::string>()->value_name("file"),  "template output (default: '-')")
-        ("context,c",     po::value<std::string>()->value_name("file"),  "template context: *.{ini,json,xml}")
-        ("engine,e",      po::value<std::string>()->value_name("name"),  "template engine: {django,ssi,tmpl}")
-        ("autoescape,a",  po::value<bool>()->value_name("bool"),         "automatically escape values (default: 'true')")
-        ("directories,d", po::value<paths>()->value_name("path"),        "template lookup directories (default: '.')")
-        ("replacement,r", po::value<std::string>()->value_name("text"),  "replaces missing values (default: '')")
-        ;
+typedef synth::processor<synth::detail::multi_template
+    < char
+    , synth::stream_template_identity
+    , synth::django::engine<>
+    , synth::ssi::engine<>
+    , synth::tmpl::engine<>
+    >
+> processor_type;
 
-    po::variables_map flags;
-    po::store(po::parse_command_line(argc, argv, description), flags);
-    po::notify(flags);
+} // namespace
 
-    if (flags.count("help")) {
-        std::cout << description << std::endl;
-        return EXIT_SUCCESS;
-    } else if (flags.count("version")) {
-        std::cout << "synth v" << AJG_SYNTH_VERSION_STRING << std::endl;
-        return EXIT_SUCCESS;
-    }
+namespace boost {
+namespace property_tree {
 
+processor_type::stream_type& operator <<( processor_type::stream_type&        output
+                                        , processor_type::context_type const& context
+                                        ) {
+    return output << "[context]";
+}
 
+}} // namespace boost::property_tree
+
+int main(int const argc, char const *const argv[])
+try {
+    ajg::synth::command<processor_type>::process(argc, argv);
     return EXIT_SUCCESS;
+} catch (std::exception const& e) {
+    std::cerr << "synth: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
---
+++ ajg/synth/command.hpp
@@ -0,0 +1,112 @@
+//  (C) Copyright 2014 Alvaro J. Genial (http://alva.ro)
+//  Use, modification and distribution are subject to the Boost Software
+//  License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt).
+
+#ifndef AJG_SYNTH_COMMAND_HPP_INCLUDED
+#define AJG_SYNTH_COMMAND_HPP_INCLUDED
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+namespace ajg {
+namespace synth {
+
+#define AJG_ARG(name, type) po::value<type##_type>()->value_name(name)
+
+template <class Processor>
+struct command {
+
+  public:
+
+    typedef Processor                                 processor_type;
+    typedef typename processor_type::char_type        char_type;
+    typedef typename processor_type::boolean_type     boolean_type;
+    typedef typename processor_type::string_type      string_type;
+    typedef typename processor_type::stream_type      stream_type;
+    typedef typename processor_type::context_type     context_type;
+    typedef typename processor_type::directories_type dirs_type;
+
+  public:
+
+    static void process(int const argc, char_type const *const argv[]) {
+        namespace po = boost::program_options;
+        namespace pt = boost::property_tree;
+
+        po::options_description description("Usage");
+
+        description.add_options()
+            ("help,h",                                  "print help message")
+            ("version,v",                               "print library version")
+         // ("input,i",       AJG_ARG("file", string),  "the source (default: '-')")            // TODO
+         // ("output,o",      AJG_ARG("file", string),  "the destination (default: '-')")       // TODO
+         // ("source,s",      AJG_ARG("text", string),  "inline alternative to input file")     // TODO
+         // ("format,f",      AJG_ARG("name", string),  "the context's format: {ini,json,xml}") // TODO
+            ("context,c",     AJG_ARG("file", string),  "the data: *.{ini,json,xml}")
+            ("engine,e",      AJG_ARG("name", string),  "template engine: {django,ssi,tmpl}")
+            ("autoescape,a",  AJG_ARG("bool", boolean), "automatically escape values (default: 'true')")
+            ("directories,d", AJG_ARG("path", dirs),    "template lookup directories (default: '.')")
+            ("replacement,r", AJG_ARG("text", string),  "replaces missing values (default: '')")
+            ;
+
+        po::variables_map flags;
+        po::store(po::parse_command_line(argc, argv, description), flags);
+        po::notify(flags);
+
+        if (flags.count("help")) {
+            std::cerr << description << std::endl;
+            return;
+        } else if (flags.count("version")) {
+            std::cerr << "synth v" << AJG_SYNTH_VERSION_STRING << std::endl;
+            return;
+        } else if (!flags.count("engine")) {
+            throw_exception(std::invalid_argument("missing engine name"));
+        }
+
+        processor_type const processor
+            ( std::cin >> std::noskipws
+            , flags["engine"].as<string_type>()
+            , flags.count("autoescape")  ? flags["autoescape"].as<boolean_type>() : boolean_type(true)
+            , flags.count("replacement") ? flags["replacement"].as<string_type>() : string_type()
+            , flags.count("directories") ? flags["directories"].as<dirs_type>()   : dirs_type()
+            );
+
+        context_type context;
+
+        if (flags.count("context")) {
+            string_type const& filename = flags["context"].as<string_type>();
+            std::basic_ifstream<char_type> file(filename);
+
+            if (boost::algorithm::ends_with(filename, synth::detail::text(".ini"))) {
+                pt::read_ini(file, context);
+            }
+            else if (boost::algorithm::ends_with(filename, synth::detail::text(".json"))) {
+                pt::read_json(file, context);
+            }
+            else if (boost::algorithm::ends_with(filename, synth::detail::text(".xml"))) {
+                pt::read_xml(file, context);
+            }
+            else {
+                throw_exception(std::invalid_argument("unknown context format"));
+            }
+        }
+
+        processor.render(std::cout, context);
+    }
+};
+
+#undef AJG_ARG
+
+}} // namespace ajg::synth
+
+#endif // AJG_SYNTH_COMMAND_HPP_INCLUDED
+

4313b9511ab8fcb734ed136abc00cba357a77235
Alvaro J. Genial <genial@alva.ro>
2014-03-03 14:06:02 -0500

    Bump version

--- ajg/synth/config.hpp
+++ ajg/synth/config.hpp
@@ -8,7 +8,7 @@
 
 #include <boost/preprocessor/stringize.hpp>
 
-#define AJG_SYNTH_VERSION        0.5.0
+#define AJG_SYNTH_VERSION        0.6.0
 #define AJG_SYNTH_VERSION_STRING BOOST_PP_STRINGIZE(AJG_SYNTH_VERSION)
 
 #ifndef AJG_SYNTH_SEQUENCE_LIMIT

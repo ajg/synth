d5b0d273f148ba67490c1f6c87c27565759fdc04
Alvaro J. Genial <genial@alva.ro>
2014-03-11 00:22:14 -0400

    Fully generalize compile-time (fusion) vectors

--- ajg/synth/bindings/python/module.cpp
+++ ajg/synth/bindings/python/module.cpp
@@ -3,6 +3,8 @@
 //  License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt).
 
+#include <ajg/synth/config.hpp> // Must come ahead of everything else.
+
 #include <boost/python.hpp>
 
 #include <ajg/synth/engines.hpp>

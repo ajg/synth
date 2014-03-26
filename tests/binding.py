9070aabae63289abaa6e23daa60446b81b6143f9
Alvaro J. Genial <genial@alva.ro>
2014-03-26 18:13:28 -0400

    Add python binding rudimentary tests

--- tests/binding.py
+++ tests/binding.py
@@ -4,60 +4,57 @@
 ##  http://www.boost.org/LICENSE_1_0.txt).
 
 import synth
+from difflib import unified_diff
+
+# TODO: Golden-file tests like these are brittle and ugly;
+#       refactor and figure out how to reuse the native unit tests.
 
 print('Loaded synth; version: ' + synth.version())
 
 source = """
-{% load foo bar qux %}
-{% load x y z from some.package %}
+True:   {{ True }}; {% if True %} GOOD {% else %} BAD {% endif %}.
+False:  {{ False }}; {% if False %} BAD {% else %} GOOD {% endif %}.
 
-True:   {{ True }}; {% if True %} GOOD {% else %} BAD {% endif %}
-False:  {{ False }}; {% if False %} BAD {% else %} GOOD {% endif %}
+0:      {{ 0 }}; {% if 0 %} BAD {% else %} GOOD {% endif %}.
+42:     {{ 42 }}; {% if 42 %} GOOD {% else %} BAD {% endif %}.
 
-0:      {{ 0 }}; {% if 0 %} BAD {% else %} GOOD {% endif %}
-42:     {{ 42 }}; {% if 42 %} GOOD {% else %} BAD {% endif %}
+"":     {{ "" }}; {% if "" %} BAD {% else %} GOOD {% endif %}.
+"test": {{ "test" }}; {% if "test" %} GOOD {% else %} BAD {% endif %}.
 
-"":     {{ "" }}; {% if "" %} BAD {% else %} GOOD {% endif %}
-"test": {{ "test" }}; {% if "test" %} GOOD {% else %} BAD {% endif %}
+bool_true: {{ bool_true }}; {% if bool_true %} GOOD {% else %} BAD {% endif %}.
+bool_false: {{ bool_false }}; {% if bool_false %} BAD {% else %} GOOD {% endif %}.
 
-bool_true: {{ bool_true }}; {% if bool_true %} GOOD {% else %} BAD {% endif %}
-bool_false: {{ bool_false }}; {% if bool_false %} BAD {% else %} GOOD {% endif %}
+int_0: {{ int_0 }}; {% if int_0 %} BAD {% else %} GOOD {% endif %}.
+int_42: {{ int_42 }}; {% if int_42 %} GOOD {% else %} BAD {% endif %}.
 
-int_0: {{ int_0 }}; {% if int_0 %} BAD {% else %} GOOD {% endif %}
-int_42: {{ int_42 }}; {% if int_42 %} GOOD {% else %} BAD {% endif %}
+str_empty: {{ str_empty }}; {#% if str_empty %} BAD {% else %} GOOD {% endif %#}.
+str_test: {{ str_test }}; {% if str_test %} GOOD {% else %} BAD {% endif %}.
 
-str_empty: {{ str_empty }}; {#% if str_empty %} BAD {% else %} GOOD {% endif %#}
-str_test: {{ str_test }}; {% if str_test %} GOOD {% else %} BAD {% endif %}
+list_empty: {{ list_empty }}; {% if list_empty %} BAD {% else %} GOOD {% endif %}.
+list_12345: {{ list_12345 }}; {% if list_12345 %} GOOD {% else %} BAD {% endif %}.
 
-list_empty: {{ list_empty }}; {% if list_empty %} BAD {% else %} GOOD {% endif %}
-list_12345: {{ list_12345 }}; {% if list_12345 %} GOOD {% else %} BAD {% endif %}
+dict_empty: {{ dict_empty }}; {% if dict_empty %} BAD {% else %} GOOD {% endif %}.
+dict_abc: {{ dict_abc }}; {% if dict_abc %} GOOD {% else %} BAD {% endif %}.
 
-dict_empty: {{ dict_empty }}; {% if dict_empty %} BAD {% else %} GOOD {% endif %}
-dict_abc: {{ dict_abc }}; {% if dict_abc %} GOOD {% else %} BAD {% endif %}
+{#% for k, v in str_empty %} BAD {% empty %} GOOD {% endfor %#}.
+{% for k, v in list_empty %} BAD {% empty %} GOOD {% endfor %}.
+{% for k, v in dict_empty %} BAD {% empty %} GOOD {% endfor %}.
 
-{#% for k, v in str_empty %} BAD {% empty %} GOOD {% endfor %#}
-{% for k, v in list_empty %} BAD {% empty %} GOOD {% endfor %}
-{% for k, v in dict_empty %} BAD {% empty %} GOOD {% endfor %}
+"test": {% for v in "test" %}{{ v }}{% endfor %}.
+str_test: {#% for v in str_test %}{{ v }}{% endfor %#}.
+list_12345: {% for v in list_12345 %}{{ v }}{% endfor %}.
+dict_abc: {% for v in dict_abc %}{{ v }}{% endfor %}.
 
-"test": {% for v in "test" %}{{ v }}{% endfor %}
-str_test: {#% for v in str_test %}{{ v }}{% endfor %#}
-list_12345: {% for v in list_12345 %}{{ v }}{% endfor %}
-dict_abc: {% for v in dict_abc %}{{ v }}{% endfor %}
+dict_abc: {% for k, v in dict_abc.items %}{{ k }}:{{ v }}{% endfor %}.
 
-dict_abc: {% for k, v in dict_abc.items %}{{ k }}:{{ v }}{% endfor %}
-
-{% comment %}
-
-{{ 0 or 0 }}
-{{ 0 and 0 }}
-{{ 0 or "foo" }}
-{{ 0 and "foo" }}
-{{ "foo" or 0 }}
-{{ "foo" and 0 }}
-{{ "foo" or "bar" }}
-{{ "foo" and "bar" }}
-
-{% endcomment %}
+{{ 0 or 0 }}.
+{{ 0 and 0 }}.
+{{ 0 or "foo" }}.
+{{ 0 and "foo" }}.
+{{ "foo" or 0 }}.
+{{ "foo" and 0 }}.
+{{ "foo" or "bar" }}.
+{{ "foo" and "bar" }}.
 
 """.encode('utf-8')
 
@@ -73,7 +70,64 @@ context = {
     'dict_empty': {},
     'dict_abc':   {'a': 'A', 'b': 'B', 'c': 'C'},
 }
+
 template = synth.Template(source, 'django')
 
-print('Parsing succeeded: ' + str(template))
-print(template.render_to_string(context))
+print('Parsing succeeded!')
+
+string = template.render_to_string(context)
+
+print('Rendering succeeded!')
+
+golden = """
+True:   True;  GOOD .
+False:  False;  GOOD .
+
+0:      0;  GOOD .
+42:     42;  GOOD .
+
+"":     ;  GOOD .
+"test": test;  GOOD .
+
+bool_true: True;  GOOD .
+bool_false: False;  GOOD .
+
+int_0: 0;  GOOD .
+int_42: 42;  GOOD .
+
+str_empty: ; .
+str_test: test;  GOOD .
+
+list_empty: [];  GOOD .
+list_12345: [1, 2, 3, 4, 5];  GOOD .
+
+dict_empty: {};  GOOD .
+dict_abc: {&apos;a&apos;: &apos;A&apos;, &apos;c&apos;: &apos;C&apos;, &apos;b&apos;: &apos;B&apos;};  GOOD .
+
+.
+ GOOD .
+ GOOD .
+
+"test": test.
+str_test: .
+list_12345: 12345.
+dict_abc: acb.
+
+dict_abc: a:Ac:Cb:B.
+
+0.
+0.
+foo.
+0.
+foo.
+0.
+foo.
+bar.
+
+"""
+
+if string != golden:
+    diff = ''.join(unified_diff(string.splitlines(True), golden.splitlines(True)))
+    raise Exception("MISMATCH:\n" + diff)
+
+print('Matching succeeded!')

911d81d1e91cef018bf95f071711251a35cb99bc
Alvaro J. Genial <genial@alva.ro>
2014-03-28 16:02:06 -0400

    Factor out version.hpp from config.hpp

---
+++ ajg/synth/version.hpp
@@ -0,0 +1,20 @@
+//  (C) Copyright 2014 Alvaro J. Genial (http://alva.ro)
+//  Use, modification and distribution are subject to the Boost Software
+//  License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt).
+
+#ifndef AJG_SYNTH_VERSION_HPP_INCLUDED
+#define AJG_SYNTH_VERSION_HPP_INCLUDED
+
+#define AJG_SYNTH_VERSION_MAJOR 0
+#define AJG_SYNTH_VERSION_MINOR 11
+#define AJG_SYNTH_VERSION_PATCH 0
+
+#define AJG_SYNTH_VERSION        AJG_SYNTH_VERSION_MAJOR.AJG_SYNTH_VERSION_MINOR.AJG_SYNTH_VERSION_PATCH
+#define AJG_SYNTH_VERSION_TUPLE  (AJG_SYNTH_VERSION_MAJOR, AJG_SYNTH_VERSION_MINOR, AJG_SYNTH_VERSION_PATCH)
+#define AJG_SYNTH_VERSION_STRING AJG_SYNTH_STRINGIZE(AJG_SYNTH_VERSION)
+
+#define AJG_SYNTH_STRINGIZE_(e) #e
+#define AJG_SYNTH_STRINGIZE(e)  AJG_SYNTH_STRINGIZE_(e)
+
+#endif // AJG_SYNTH_VERSION_HPP_INCLUDED

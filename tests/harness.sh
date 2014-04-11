b8e889eb34ecc0f9f975ab097aee1eca30837026
Alvaro J. Genial <genial@alva.ro>
2014-04-10 21:34:35 -0400

    Fix test harness

--- tests/harness.sh
+++ tests/harness.sh
@@ -7,9 +7,9 @@ function main {
     expect=$(echo -e -n 'foo: 1\nbar: 2\nqux: 3\n')
     actual=$(cat tests/templates/django/variables.tpl | ./synth -e django -c tests/data/variables.json)
 
-    if [[ expect == actual ]]
+    if [[ "$expect" == "$actual" ]]
     then echo -e 'Success';
-    else echo -e "Failure\nExpect: `$expect`\nActual: `$actual`";
+    else echo -e 'Failure\nExpect: `'"$expect"'`\nActual: `'"$actual"'`';
     fi
 }
 

d9d1bfe0b0b48a7c37ec128cd56d7e4d0e1f9dcd
Alvaro J. Genial <genial@alva.ro>
2014-04-15 22:41:05 -0400

    Split out python binding into separate files

---
+++ ajg/synth/bindings/python/loader.hpp
@@ -0,0 +1,40 @@
+//  (C) Copyright 2014 Alvaro J. Genial (http://alva.ro)
+//  Use, modification and distribution are subject to the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt).
+
+#ifndef AJG_SYNTH_BINDINGS_PYTHON_LOADER_HPP_INCLUDED
+#define AJG_SYNTH_BINDINGS_PYTHON_LOADER_HPP_INCLUDED
+
+#include <ajg/synth/bindings/python/detail.hpp>
+#include <ajg/synth/bindings/python/library.hpp>
+
+namespace ajg {
+namespace synth {
+namespace python {
+
+//
+// loader
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <class Options>
+struct loader : Options::abstract_loader_type {
+    typedef Options                                      options_type;
+    typedef typename options_type::string_type           string_type;
+    typedef typename options_type::library_type          library_type;
+
+    explicit loader(py::object const& object) : object_(object) {}
+    virtual ~loader() {}
+
+    virtual library_type load_library(string_type const& name) {
+        return library_type(new library<options_type>(object_(name)));
+    }
+
+
+  private:
+
+    py::object /*const*/ object_;
+};
+
+}}} // namespace ajg::synth::python
+
+#endif // AJG_SYNTH_BINDINGS_PYTHON_LOADER_HPP_INCLUDED

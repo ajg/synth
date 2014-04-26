8558f619886ce4f84e75ce50221acb497b2dbc1b
Alvaro J. Genial <genial@alva.ro>
2014-04-26 17:29:06 -0400

    Add basic Homebrew formula

---
+++ synth.rb
@@ -0,0 +1,30 @@
+require "formula"
+
+class Synth < Formula
+  homepage "https://github.com/ajg/synth"
+  head "https://github.com/ajg/synth.git"
+  url "https://github.com/ajg/synth/archive/v0.52.0.tar.gz"
+  sha1 "a5ba180c0d8a53c67859da1b49958352c4d807f7"
+
+  depends_on "scons"  => :build
+  depends_on "python" => :optional
+  depends_on "boost" if build.without? "python"
+  depends_on "boost"  => "with-python" if build.with? "python"
+  # TODO: boost option {auto,local,system}
+
+  def install
+    scons "synth", "debug=0", "boost=system"
+    bin.install "synth"
+    # TODO: include.install "ajg/synth.hpp", Dir["ajg/**/synth.hpp"]
+
+    if build.with? "python"
+      system "python", "setup.py", "install", "--prefix=#{prefix}"
+    end
+  end
+
+  # TODO: bottle do ...
+
+  test do
+    system "#{bin}/synth", "--version"
+  end
+end

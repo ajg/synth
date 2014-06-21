26f02ad604e2f43e36c68bbe22ffb603910949a3
Alvaro J. Genial <genial@alva.ro>
2014-06-20 21:37:01 -0400

    Add tmpl python example

---
+++ examples/simple_tmpl.py
@@ -0,0 +1,11 @@
+
+import synth, sys
+
+def simple_tmpl_example():
+    t = synth.Template('Howdy, <TMPL_VAR user>!', 'tmpl')
+    c = {'user': 'Dolph Lundgren'}
+
+    # Render to different destinations:
+    t.render_to_path("greeting.txt", c)
+    t.render_to_file(sys.stdout, c)
+    print t.render_to_string(c)
